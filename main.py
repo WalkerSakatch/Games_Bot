dfc6ff17608f57ef142c01a0ce114038240287b3
Walker Sakatch <walkersakatch@gmail.com>
2021-05-19 11:39:41 -0600

    added underscore lmao

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ def nba(bot, update):
 
 def dice(bot, update):
 	msg_ID = update.message.message_id
-	bot.send_dice(chatid=update.message.chat_id, reply_to_message_id=msg_ID, emoji=u'\U0001F3B2')
+	bot.send_dice(chat_id=update.message.chat_id, reply_to_message_id=msg_ID)
 
 #command /mhw makes the bot tag everyone in the chat that plays Monster hunter world by their telegram name
 def mhw(bot, update):
