891f205c6031e04a025866d4c00c619f15889bc6
Walker Sakatch <walkersakatch@gmail.com>
2021-05-26 15:44:53 -0600

    context lul

--- db.py
+++ db.py
@@ -12,21 +12,21 @@ def submit(string):
     elif(string in fortunes.values()):
         return("I've already got that one, babe")
 
-def fortune(bot, update):
+def fortune(update, context):
     global fortune_num
     msg_ID = update.message.message_id
     fortunes = firebase.get('/fortunes', None)
     if(fortunes is None):
         return("Sorry I'm all out of fortunes, come back later.")
-        bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=msg_ID, text="Sorry I'm all out of fortunes, come back later.")
+        context.bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=msg_ID, text="Sorry I'm all out of fortunes, come back later.")
     else:
         fortune = fortunes.values()
         fortune = list(fortune)
-        bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=msg_ID, text=fortune[fortune_num])
+        context.bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=msg_ID, text=fortune[fortune_num])
         fortune_num += random.randint(1, 2)
         if(fortune_num >= len(fortune) - 1):
             fortune_num = random.randint(1, 4)
-        #bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=msg_ID, text="New fortune num: {}".format(fortune_num))
+        #context.bot.send_message(chat_id=update.message.chat_id, reply_to_message_id=msg_ID, text="New fortune num: {}".format(fortune_num))
 
 def kill(string):
     firebase.delete('/fortunes', string)
